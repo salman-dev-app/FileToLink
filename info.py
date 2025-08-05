75642f1d5117aa45f99e543907dbda209ecb633d
MD SALMAN ❤️‍🩹 <mdsalmanhelp0@gmail.com>
2025-08-05 07:07:12 +0600

    Update info.py

--- info.py
+++ info.py
@@ -31,6 +31,6 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gkglkvhjkjghjj:iFJHfgT
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', false)) # Set True Or False
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
 SHORTLINK_API = environ.get('SHORTLINK_API', '')
